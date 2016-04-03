2d4e73131067e52877ac3defc6f2ea8538dea2fa
weiHelloWorld <wei.herbert.chen@gmail.com>
2016-04-02 20:36:17 -0500

    1. update Makefile, 2. add tests, 3. add .gitignore

--- openmmapi/tests/test_ANN_Force.cpp
+++ openmmapi/tests/test_ANN_Force.cpp
@@ -9,15 +9,17 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <cassert>
 
 using namespace OpenMM;
 using std::string;
 using std::vector;
 using std::cout;
+using std::endl;
 
 const double TOL = 1e-5;
 
-static void test_setters_getters() {
+static void test_setters_getters_1() {
     ANN_Force my_force;
     int num_of_nodes[NUM_OF_LAYERS] = {76, 10, 2};
     my_force.set_num_of_nodes(num_of_nodes);
@@ -27,12 +29,30 @@ static void test_setters_getters() {
     return;
 }
 
+static void test_setters_getters_2() {
+    ANN_Force my_force;
+    vector<string> types;
+    types.push_back("Tanh1");
+    types.push_back("Tanh2");
+    types.push_back("Tanh3");
+    my_force.set_layer_types(types);
+    auto out_types = my_force.get_layer_types();
+    for(int i = 0; i < 3; i ++) {
+        // cout << out_types[i] << endl;
+        assert(out_types[i].compare(types[i]) == 0);
+    }
+    return;
+
+}
+
+
 
 int main(int numberOfArguments, char* argv[]) {
 
     try {
         std::cout << "running test for ANN_Force..." << std::endl;
-        test_setters_getters();
+        test_setters_getters_1();
+        test_setters_getters_2();
     }
     catch(const std::exception& e) {
         std::cout << "exception: " << e.what() << std::endl;
